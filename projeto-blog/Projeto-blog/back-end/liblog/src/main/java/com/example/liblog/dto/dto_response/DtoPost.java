3303db29f7dee1653633826e8dd444c6fb53202f
Veetorio <ettorevitorio98@gmail.com>
2024-01-10 19:09:43 -0300

    cgj

--- projeto-blog/Projeto-blog/back-end/liblog/src/main/java/com/example/liblog/dto/dto_response/DtoPost.java
+++ projeto-blog/Projeto-blog/back-end/liblog/src/main/java/com/example/liblog/dto/dto_response/DtoPost.java
@@ -5,8 +5,15 @@ import com.example.liblog.models.Post;
 import java.time.LocalDate;
 
 
-public record DtoPost(String title, String comentario, String url, LocalDate data, String hoursdate) {
+public record DtoPost(String title, String comentario, String url, LocalDate data, String hoursdate,String IdN) {
     public DtoPost(Post post){
-        this(post.getNome(), post.getComentario(), post.getUrl(), post.getData(), post.getHoursdate());
+        this(
+                post.getNome()
+                        .replaceAll("-"," ")
+                        .substring(0,post.getNome().length() - 5),
+                post.getComentario(),
+                post.getUrl(), post.getData(),
+                post.getHoursdate(),
+                post.getNome().substring(post.getNome().length() - 5));
     }
 }
